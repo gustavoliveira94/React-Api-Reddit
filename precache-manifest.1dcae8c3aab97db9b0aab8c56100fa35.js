5f7dcf42563f06b70c4df6981b7c2d82c5194135
Gustavo Oliveira <gusttavo@live.com>
2018-12-02 04:01:29 -0200

    Updates

---
+++ precache-manifest.1dcae8c3aab97db9b0aab8c56100fa35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ffdbe9b247676acb0e8",
+    "url": "/React-Api-Reddit/static/js/runtime~main.2ffdbe9b.js"
+  },
+  {
+    "revision": "227b1aa11201b18084bb",
+    "url": "/React-Api-Reddit/static/js/main.227b1aa1.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/React-Api-Reddit/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "227b1aa11201b18084bb",
+    "url": "/React-Api-Reddit/static/css/main.18df2e64.chunk.css"
+  },
+  {
+    "revision": "e3cf4cd3a6bf2a9b9720eb43e2a2c6cc",
+    "url": "/React-Api-Reddit/index.html"
+  }
+];
