4c8eb6bcee8e36fa34fae90e9cdad28e87a524af
Gustavo Oliveira <gustavo.oliveira@avantare.intranet>
2018-12-03 16:31:08 -0200

    Rotas

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,9 +6,9 @@ class Options extends Component {
     render() {
         return (
             <div className="App-options">
-                <Link to="https://gustavoliveira94.github.io/react-api-reddit/hot" onClick={this.props.changeHot} style={{ background: this.props.colorHot }}>HOT</Link>
-                <Link to="https://gustavoliveira94.github.io/react-api-reddit/news" onClick={this.props.changeNews} style={{ background: this.props.colorNews }}>NEWS</Link>
-                <Link to="https://gustavoliveira94.github.io/react-api-reddit/rising" onClick={this.props.changeRising} style={{ background: this.props.colorRising }}>RISING</Link>
+                <Link to="/react-api-reddit/hot" onClick={this.props.changeHot} style={{ background: this.props.colorHot }}>HOT</Link>
+                <Link to="/react-api-reddit/news" onClick={this.props.changeNews} style={{ background: this.props.colorNews }}>NEWS</Link>
+                <Link to="/react-api-reddit/rising" onClick={this.props.changeRising} style={{ background: this.props.colorRising }}>RISING</Link>
             </div>
         );
     }
